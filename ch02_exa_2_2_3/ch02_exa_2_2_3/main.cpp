ae53c5ba43f1307c2a5d445b999ed4a76d5247a4
LuaGanrqunx <aqunxgm@gmail.com>
2020-06-14 14:53:21 +0800

    message

--- ch02_exa_2_2_3/ch02_exa_2_2_3/main.cpp
+++ ch02_exa_2_2_3/ch02_exa_2_2_3/main.cpp
@@ -5,11 +5,20 @@
 //  Created by Ganrqunx on 2020/6/14.
 //  Copyright © 2020 Ganrqunx. All rights reserved.
 //
-
+/*
+ 本程式展示C++ 資料過大超成溢位的效果
+ */
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //cout << "Hello, World!\n";
+    short id1 = 32767;
+    short id = 32768;
+    int id2 = 32768;
+    cout << "id = " << id << endl;
+    cout << "id1 = " << id1 << endl;
+    cout << "id2 = " << id2 << endl;
     return 0;
 }
