cecf9f0599fd0e55555bfdc0aa78c2a49f3d052b
LuaGanrqunx <aqunxgm@gmail.com>
2020-06-14 14:18:58 +0800

    message

---
+++ ch02_exa_2_2_3/ch02_exa_2_2_3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ch02_exa_2_2_3
+//
+//  Created by Ganrqunx on 2020/6/14.
+//  Copyright © 2020 Ganrqunx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
